c8f0fd58ee3cdcc67274f27d17177dca480e350c
Alberto Primerano <alberto.primerano@gmail.com>
2021-04-15 10:05:37 +0000

    Completed for review

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-
-
-
-
+//Display the rate value next to the rate range 
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval+"%";
 }
 
-
+//Calculate the amount to be displayed and define the output message
 function compute()
 {
+    //Get all the data required from document
     var principal = document.getElementById("principal").value;  
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
-    var amount = parseFloat(principal)+ parseFloat(interest)
-    text = document.createTextNode(''+intValue);
-    document.documentElement.innerHTML = '';
-    var span = document.getElementById("result").innerHTML = '';
-    span.appendChild(text)
-    //document.getElementById("result").innerText = "If you deposit "  + principal +",\<br/\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    //Returns an error if amount inserted is negative or not inserted
+    if (parseFloat(principal) <= 0 || principal == ""){
+        alert("Enter a positive number")
+    }
+    else{
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        //Determine interest
+        var interest = principal * years * rate /100;
+        //Determine the correct year
+        var year = new Date().getFullYear()+parseInt(years);
+        //Calculate the amount (rounded to 2 decimal cyphers)
+        var amount = (parseFloat(principal)+ parseFloat(interest)).toFixed(2)
+        //Set the message to be displayed
+        var p = document.getElementById("result")
+        p.innerHTML = "If you deposit <mark>"  + principal +"</mark>,\<br/\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+    }
 }
         
